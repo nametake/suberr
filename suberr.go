efd678f5b5df4a4dfdd97c04b3034f666eca37d9
nametake <nametake.kyarabuki@gmail.com>
2018-06-27 01:07:13 +0900

    Add 'Add' function

---
+++ suberr.go
@@ -0,0 +1,34 @@
+// Package suberr provides ...
+package suberr
+
+import "fmt"
+
+func Add(main, sub error) error {
+	return &subError{
+		main: main,
+		sub:  sub,
+	}
+}
+
+type causer interface {
+	Cause() error
+}
+
+type getter interface {
+	Get() error
+}
+
+var (
+	_ error = (*subError)(nil)
+	// _ fmt.Formatter = (*subError)(nil)
+	// _ causer = (*subError)(nil)
+	// _ getter = (*subError)(nil)
+)
+
+type subError struct {
+	main, sub error
+}
+
+func (s *subError) Error() string {
+	return fmt.Sprintf("%v: %v", s.main, s.sub)
+}
